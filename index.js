cb8a2fe4bcba4334ba33deadf333d4d1a4ea9d18
aymensellaouti <aymen.sellaouti@gmail.com>
2026-02-05 16:24:01 +0100

    seance du 5 2 2025 JS 1

---
+++ index.js
@@ -0,0 +1,31 @@
+// console.log('1');
+
+// setTimeout(() => {
+//     console.log('2');
+// }, 0);
+
+// console.log('3');
+// const t = [11, 1, 2, 4]
+
+// console.log(t.sort(
+//     (a, b) => a-b
+// ));
+
+const sensors = [
+    { id: "TEMP_01", type: "temperature", value: 22.5, status: "active" },
+    { id: "POLL_01", type: "pollution", value: 85, status: "active" },
+    { id: "HUM_01", type: "temperature", value: 45, status: "inactive" },
+    { id: "TEMP_02", type: "temperature", value: 31.0, status: "active" },
+    { id: "POLL_02", type: "pollution", value: 42, status: "active" },
+    { id: "TEMP_03", type: "temperature", value: -5.0, status: "faulty" }
+]; 
+// Calculer la température moyenne de tous les capteurs de type "temperature" 
+// (uniquement ceux qui ne sont pas "faulty").
+const nonFaultyTemperatureSensors = sensors.filter(
+    (sensor) => sensor.type == 'temperature' && sensor.status != 'faulty'
+);
+//console.log(nonFaultyTemperatureSensors);
+
+const avgNonFaultyTemperatureSensors = nonFaultyTemperatureSensors.length ? nonFaultyTemperatureSensors.reduce((acc, element) => acc + element.value, 0)/nonFaultyTemperatureSensors.length : 0;
+
+console.log(avgNonFaultyTemperatureSensors);
