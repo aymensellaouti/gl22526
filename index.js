c529a4e9f46d18ebabac41495d7f64aeeb000647
aymensellaouti <aymen.sellaouti@gmail.com>
2026-02-12 16:27:05 +0100

    s&ance numéro 3 du 12 2 2026

--- index.js
+++ index.js
@@ -11,21 +11,44 @@
 //     (a, b) => a-b
 // ));
 
-const sensors = [
-    { id: "TEMP_01", type: "temperature", value: 22.5, status: "active" },
-    { id: "POLL_01", type: "pollution", value: 85, status: "active" },
-    { id: "HUM_01", type: "temperature", value: 45, status: "inactive" },
-    { id: "TEMP_02", type: "temperature", value: 31.0, status: "active" },
-    { id: "POLL_02", type: "pollution", value: 42, status: "active" },
-    { id: "TEMP_03", type: "temperature", value: -5.0, status: "faulty" }
-]; 
-// Calculer la température moyenne de tous les capteurs de type "temperature" 
-// (uniquement ceux qui ne sont pas "faulty").
-const nonFaultyTemperatureSensors = sensors.filter(
-    (sensor) => sensor.type == 'temperature' && sensor.status != 'faulty'
+// const sensors = [
+//     { id: "TEMP_01", type: "temperature", value: 22.5, status: "active" },
+//     { id: "POLL_01", type: "pollution", value: 85, status: "active" },
+//     { id: "HUM_01", type: "temperature", value: 45, status: "inactive" },
+//     { id: "TEMP_02", type: "temperature", value: 31.0, status: "active" },
+//     { id: "POLL_02", type: "pollution", value: 42, status: "active" },
+//     { id: "TEMP_03", type: "temperature", value: -5.0, status: "faulty" }
+// ]; 
+// // Calculer la température moyenne de tous les capteurs de type "temperature" 
+// // (uniquement ceux qui ne sont pas "faulty").
+// const nonFaultyTemperatureSensors = sensors.filter(
+//     (sensor) => sensor.type == 'temperature' && sensor.status != 'faulty'
+// );
+// //console.log(nonFaultyTemperatureSensors);
+
+// const avgNonFaultyTemperatureSensors = nonFaultyTemperatureSensors.length ? nonFaultyTemperatureSensors.reduce((acc, element) => acc + element.value, 0)/nonFaultyTemperatureSensors.length : 0;
+
+// console.log(avgNonFaultyTemperatureSensors);
+// select ampoule 
+const ampoule = document.querySelector('div');
+setInterval(
+    () => {
+        ampoule.classList.toggle('close');
+        ampoule.classList.toggle('open');
+    }, 1500
 );
-//console.log(nonFaultyTemperatureSensors);
-
-const avgNonFaultyTemperatureSensors = nonFaultyTemperatureSensors.length ? nonFaultyTemperatureSensors.reduce((acc, element) => acc + element.value, 0)/nonFaultyTemperatureSensors.length : 0;
-
-console.log(avgNonFaultyTemperatureSensors);
+const regions = ['Djerba', 'Tunis', 'Sousse', 'Sfax', 'Kasserine', 'Beja'];
+const ol = document.querySelector('.regions');
+let i = 0;
+setInterval(
+    () => {
+        if(!(i % regions.length)) {
+            // remet à jour la liste
+            ol.innerHTML = '';
+        }
+        // nzid une région fel liste 
+        const region = `<li>${regions[i % regions.length]}</li>`;
+        i++;
+        ol.innerHTML+= region;
+    }, 1000
+)
